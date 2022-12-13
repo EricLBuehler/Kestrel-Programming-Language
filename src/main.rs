17c87bc2373da75d1be764f5bc41df28eb40f925
EricLBuehler <ericlbuehler@gmail.com>
2022-12-13 14:51:05 -0500

    Add codegen module and lexer driver code

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,49 @@
-use inkwell::builder::Builder;
-use inkwell::context::Context;
-use inkwell::module::Module;
-use inkwell::passes::PassManagerSubType;
+mod lexer;
+mod codegen;
 
-use std::error::Error;
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
 
-extern crate guess_host_triple;
-
-struct CodeGen<'ctx> {
-    context: &'ctx Context,
-    module: Module<'ctx>,
-    builder: Builder<'ctx>,
-}
-
-impl<'ctx> CodeGen<'ctx> {
-    fn compile(&self) {
-        let i8_type: inkwell::types::IntType = self.context.i8_type();
-        let i32_type: inkwell::types::IntType = self.context.i32_type();
-        let char_ptr: inkwell::types::PointerType = i8_type.ptr_type(inkwell::AddressSpace::Generic);
-
-        let printf_type: inkwell::types::FunctionType = i32_type.fn_type(&[char_ptr.into()], true);
-        let printf: inkwell::values::FunctionValue = self.module.add_function("printf", printf_type, Some(inkwell::module::Linkage::External));
-        
-        let fn_type: inkwell::types::FunctionType = i32_type.fn_type(&[], false);
-        let function: inkwell::values::FunctionValue = self.module.add_function("main", fn_type, None);
-        let basic_block: inkwell::basic_block::BasicBlock = self.context.append_basic_block(function, "entry");
-
-        let mut attr: inkwell::attributes::Attribute = self.context.create_enum_attribute(inkwell::attributes::Attribute::get_named_enum_kind_id("noinline"), 0);
-
-        function.add_attribute(inkwell::attributes::AttributeLoc::Function, attr);
-
-        attr = self.context.create_enum_attribute(inkwell::attributes::Attribute::get_named_enum_kind_id("optnone"), 0);
-
-        function.add_attribute(inkwell::attributes::AttributeLoc::Function, attr);
-
-        self.builder.position_at_end(basic_block);    
-        
-        let stdin: std::io::Stdin = std::io::stdin();
-        let mut msg: String = String::from("");
-        println!("Enter something to print: ");
-        stdin.read_line(&mut msg).expect("Unable to read user input.");
-        msg.push('\0');
-
-        let arr_type: inkwell::types::ArrayType = self.context.i8_type().array_type(msg.len()  as u32);
-        
-        
-        let global: inkwell::values::GlobalValue = self.module.add_global(arr_type, None, "mystring");
-        
-        
-        let mut arr: Vec<u8> = Vec::new();
-        for chr in msg.as_bytes() {
-            arr.push(chr.clone());
-        }
-
-        let vec: Vec<inkwell::values::IntValue> = arr.iter().map(|&x| {i8_type.const_int(x as u64, false)} ).collect::<Vec<_>>();
-
-        let arr: inkwell::values::ArrayValue = i8_type.const_array(&vec[..]);
-
-        global.set_initializer(&arr);
-        global.set_constant(true);
-        global.set_alignment(std::mem::size_of::<u8>() as u32);
-        global.set_visibility(inkwell::GlobalVisibility::Default);
-        global.set_unnamed_address(inkwell::values::UnnamedAddress::Global);
-        
-        let value: inkwell::values::PointerValue = unsafe { self.builder.build_in_bounds_gep(global.as_pointer_value(), &[i32_type.const_int(0, false), i32_type.const_int(0, false)], "val") };
-
-        self.builder.build_call(printf, &[value.into()], "printf");
-
-        self.builder.build_return(Some(&i32_type.const_int(0, false),));
+    if args.len()!=2 {
+        println!("Invalid number of command line arguments. Expected 2, got {}.", args.len());
+        return;
     }
-}
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let context: inkwell::context::Context = Context::create();
-    let module: inkwell::module::Module = context.create_module("module");
+    let filename: &String = &args[1];
+    let file_data: String;
     
-    let mut triple: String = String::from("");
-    guess_host_triple::guess_host_triple()
-    .map(|t| triple = String::from(t))
-    .unwrap_or_else(|| triple = String::from("unknown-unknown-unknown"));
+    let res: Result<String, std::io::Error> = std::fs::read_to_string(filename);
+    match res {
+        Ok(_) => {
+            file_data = res.unwrap();
+        }
+        Err(_) => {
+            println!("File '{}' is unable to be opened or read.", filename);
+            return;
+        }
+    }
 
-    module.set_triple(&inkwell::targets::TargetTriple::create(triple.as_str()));
-    module.set_source_file_name("<module>");
+    let file_data_bytes: &[u8] = file_data.as_bytes();
 
-    let codegen: CodeGen = CodeGen {
-        context: &context,
-        module: module,
-        builder: context.create_builder(),
+    let lexer: lexer::Lexer = lexer::Lexer{
+        idx: 0,
+        data: file_data_bytes,
+        current: file_data_bytes[0] as char,
+        len: file_data_bytes.len(),
     };
 
-    let pass_manager_builder: inkwell::passes::PassManagerBuilder = inkwell::passes::PassManagerBuilder::create();
-    pass_manager_builder.set_optimization_level(inkwell::OptimizationLevel::Aggressive);
-    let manager: inkwell::passes::PassManager<Module> = inkwell::passes::PassManager::create(());
-    pass_manager_builder.populate_module_pass_manager(&manager);
+    let (ntok, tokens) = lexer::generate_tokens(lexer);
 
-    codegen.compile();
+    lexer::print_tokens(ntok, tokens);
 
-    unsafe { codegen.module.run_in_pass_manager(&manager) };
+    let res: Result<(), Box<dyn std::error::Error>> = codegen::generate_code("module", filename.as_str());
 
-    codegen.module.print_to_file(std::path::Path::new("output.ll"))?;
+    match res {
+        Ok(_) => {}
 
-    Ok(())
+        Err(err) => {
+            panic!("{}",err.to_string());
+        }
+    }
+
 }
