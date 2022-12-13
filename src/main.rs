986b9be40ad59a96393b223b5492fb215ce4e949
EricLBuehler <ericlbuehler@gmail.com>
2022-12-13 09:37:18 -0500

    Add explicit type annotations

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,18 @@ struct CodeGen<'ctx> {
 
 impl<'ctx> CodeGen<'ctx> {
     fn compile(&self) {
-        let i8_type = self.context.i8_type();
-        let i32_type = self.context.i32_type();
-        let char_ptr= i8_type.ptr_type(inkwell::AddressSpace::Generic);
+        let i8_type: inkwell::types::IntType = self.context.i8_type();
+        let i32_type: inkwell::types::IntType = self.context.i32_type();
+        let char_ptr: inkwell::types::PointerType = i8_type.ptr_type(inkwell::AddressSpace::Generic);
 
-        let printf_type = i32_type.fn_type(&[char_ptr.into()], true);
-        let printf = self.module.add_function("printf", printf_type, Some(inkwell::module::Linkage::External));
+        let printf_type: inkwell::types::FunctionType = i32_type.fn_type(&[char_ptr.into()], true);
+        let printf: inkwell::values::FunctionValue = self.module.add_function("printf", printf_type, Some(inkwell::module::Linkage::External));
         
-        let fn_type = i32_type.fn_type(&[], false);
-        let function = self.module.add_function("main", fn_type, None);
-        let basic_block = self.context.append_basic_block(function, "entry");
+        let fn_type: inkwell::types::FunctionType = i32_type.fn_type(&[], false);
+        let function: inkwell::values::FunctionValue = self.module.add_function("main", fn_type, None);
+        let basic_block: inkwell::basic_block::BasicBlock = self.context.append_basic_block(function, "entry");
 
-        let mut attr = self.context.create_enum_attribute(inkwell::attributes::Attribute::get_named_enum_kind_id("noinline"), 0);
+        let mut attr: inkwell::attributes::Attribute = self.context.create_enum_attribute(inkwell::attributes::Attribute::get_named_enum_kind_id("noinline"), 0);
 
         function.add_attribute(inkwell::attributes::AttributeLoc::Function, attr);
 
@@ -36,26 +36,26 @@ impl<'ctx> CodeGen<'ctx> {
 
         self.builder.position_at_end(basic_block);    
         
-        let stdin = std::io::stdin();
-        let mut msg = String::from("");
+        let stdin: std::io::Stdin = std::io::stdin();
+        let mut msg: String = String::from("");
         println!("Enter something to print: ");
         stdin.read_line(&mut msg).expect("Unable to read user input.");
         msg.push('\0');
 
-        let arr_type = self.context.i8_type().array_type(msg.len()  as u32);
+        let arr_type: inkwell::types::ArrayType = self.context.i8_type().array_type(msg.len()  as u32);
         
         
-        let global = self.module.add_global(arr_type, None, "mystring");
+        let global: inkwell::values::GlobalValue = self.module.add_global(arr_type, None, "mystring");
         
         
-        let mut arr = Vec::new();
+        let mut arr: Vec<u8> = Vec::new();
         for chr in msg.as_bytes() {
             arr.push(chr.clone());
         }
 
-        let vec = arr.iter().map(|&x| {i8_type.const_int(x as u64, false)} ).collect::<Vec<_>>();
+        let vec: Vec<inkwell::values::IntValue> = arr.iter().map(|&x| {i8_type.const_int(x as u64, false)} ).collect::<Vec<_>>();
 
-        let arr = i8_type.const_array(&vec[..]);
+        let arr: inkwell::values::ArrayValue = i8_type.const_array(&vec[..]);
 
         global.set_initializer(&arr);
         global.set_constant(true);
@@ -63,7 +63,7 @@ impl<'ctx> CodeGen<'ctx> {
         global.set_visibility(inkwell::GlobalVisibility::Default);
         global.set_unnamed_address(inkwell::values::UnnamedAddress::Global);
         
-        let value = unsafe { self.builder.build_in_bounds_gep(global.as_pointer_value(), &[i32_type.const_int(0, false), i32_type.const_int(0, false)], "val") };
+        let value: inkwell::values::PointerValue = unsafe { self.builder.build_in_bounds_gep(global.as_pointer_value(), &[i32_type.const_int(0, false), i32_type.const_int(0, false)], "val") };
 
         self.builder.build_call(printf, &[value.into()], "printf");
 
@@ -72,10 +72,10 @@ impl<'ctx> CodeGen<'ctx> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let context = Context::create();
-    let module = context.create_module("module");
+    let context: inkwell::context::Context = Context::create();
+    let module: inkwell::module::Module = context.create_module("module");
     
-    let mut triple = String::from("");
+    let mut triple: String = String::from("");
     guess_host_triple::guess_host_triple()
     .map(|t| triple = String::from(t))
     .unwrap_or_else(|| triple = String::from("unknown-unknown-unknown"));
@@ -83,15 +83,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     module.set_triple(&inkwell::targets::TargetTriple::create(triple.as_str()));
     module.set_source_file_name("<module>");
 
-    let codegen = CodeGen {
+    let codegen: CodeGen = CodeGen {
         context: &context,
         module: module,
         builder: context.create_builder(),
     };
 
-    let pass_manager_builder = inkwell::passes::PassManagerBuilder::create();
+    let pass_manager_builder: inkwell::passes::PassManagerBuilder = inkwell::passes::PassManagerBuilder::create();
     pass_manager_builder.set_optimization_level(inkwell::OptimizationLevel::Aggressive);
-    let manager = inkwell::passes::PassManager::create(());
+    let manager: inkwell::passes::PassManager<Module> = inkwell::passes::PassManager::create(());
     pass_manager_builder.populate_module_pass_manager(&manager);
 
     codegen.compile();
