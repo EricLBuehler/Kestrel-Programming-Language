bcf8484708dfe60285cbe6cf56d0b843209440f1
EricLBuehler <ericlbuehler@gmail.com>
2022-12-15 15:23:16 -0500

    Code generation for i32 mul, sub, div

--- src/codegen/builtin_types/i32type.rs
+++ src/codegen/builtin_types/i32type.rs
@@ -20,9 +20,57 @@ fn i32_add<'a>(codegen: &codegen::CodeGen<'a>, args: Vec<&Data<'a>>, pos: &parse
     };
 }
 
+fn i32_mul<'a>(codegen: &codegen::CodeGen<'a>, args: Vec<&Data<'a>>, pos: &parser::Position) -> Data<'a> {
+    if args.get(1).unwrap().tp != DataType::I32 {
+        let fmt: String = format!("invalid types for i32 +, got {} and {}", DataType::I32, args.get(1).unwrap().tp);
+        errors::raise_error(&fmt, errors::ErrorType::InvalidDataTypes, pos, codegen.info);
+    }
+    
+    let selfv: inkwell::values::IntValue = args.get(0).unwrap().data.unwrap().into_int_value();
+    let otherv: inkwell::values::IntValue = args.get(1).unwrap().data.unwrap().into_int_value();
+
+    return Data {
+        data: Some(inkwell::values::AnyValueEnum::IntValue(codegen.builder.build_int_mul(selfv, otherv, "i32sum"))),
+        tp: DataType::I32,
+    };
+}
+
+fn i32_sub<'a>(codegen: &codegen::CodeGen<'a>, args: Vec<&Data<'a>>, pos: &parser::Position) -> Data<'a> {
+    if args.get(1).unwrap().tp != DataType::I32 {
+        let fmt: String = format!("invalid types for i32 +, got {} and {}", DataType::I32, args.get(1).unwrap().tp);
+        errors::raise_error(&fmt, errors::ErrorType::InvalidDataTypes, pos, codegen.info);
+    }
+    
+    let selfv: inkwell::values::IntValue = args.get(0).unwrap().data.unwrap().into_int_value();
+    let otherv: inkwell::values::IntValue = args.get(1).unwrap().data.unwrap().into_int_value();
+
+    return Data {
+        data: Some(inkwell::values::AnyValueEnum::IntValue(codegen.builder.build_int_sub(selfv, otherv, "i32sum"))),
+        tp: DataType::I32,
+    };
+}
+
+fn i32_div<'a>(codegen: &codegen::CodeGen<'a>, args: Vec<&Data<'a>>, pos: &parser::Position) -> Data<'a> {
+    if args.get(1).unwrap().tp != DataType::I32 {
+        let fmt: String = format!("invalid types for i32 +, got {} and {}", DataType::I32, args.get(1).unwrap().tp);
+        errors::raise_error(&fmt, errors::ErrorType::InvalidDataTypes, pos, codegen.info);
+    }
+    
+    let selfv: inkwell::values::IntValue = args.get(0).unwrap().data.unwrap().into_int_value();
+    let otherv: inkwell::values::IntValue = args.get(1).unwrap().data.unwrap().into_int_value();
+
+    return Data {
+        data: Some(inkwell::values::AnyValueEnum::IntValue(codegen.builder.build_int_signed_div(selfv, otherv, "i32sum"))),
+        tp: DataType::I32,
+    };
+}
+
 pub fn init_i32(codegen: &mut codegen::CodeGen) {
     let mut traits: HashMap<String, Trait> = HashMap::new();
     traits.insert(TraitType::Add.to_string(), builtin_types::create_trait(i32_add, 2, TraitType::Add, String::from("i32"), DataType::I32));
+    traits.insert(TraitType::Mul.to_string(), builtin_types::create_trait(i32_mul, 2, TraitType::Mul, String::from("i32"), DataType::I32));
+    traits.insert(TraitType::Sub.to_string(), builtin_types::create_trait(i32_sub, 2, TraitType::Sub, String::from("i32"), DataType::I32));
+    traits.insert(TraitType::Div.to_string(), builtin_types::create_trait(i32_div, 2, TraitType::Div, String::from("i32"), DataType::I32));
 
     builtin_types::add_simple_type(codegen, traits, DataType::I32, "i32");
 }
